18783fa61e6498d13610622fce4c26970e88001c
JatinKumarParashar <vkp8015@gmail.com>
2022-08-26 12:26:19 +0530

    Task 12 (Advanced JS)

--- demo.js
+++ demo.js
@@ -1,62 +1,53 @@
-/*
-var obj={
-    val:2
-}
 
-function sum(x,y){
-    return this.val+x+y;
-}
-*/
-/*  call function
-console.log(sum.call(obj,3,4));
-*/
-
-
-/*apply function
-const arr=[3,4];
-console.log(sum.apply(obj,arr));
-*/
-
-/*bind function
-var bound=sum.bind(obj);
-console.log(bound(3,4));
-*/
-
-/*
-var Student={
-    age:20
-}
-function print(){
-    return this.age;
-}
-var bound=print.bind(Student);
-console.log(bound());
-*/
-
-/* Currying
-// Method 1
-
-let multiply=function(x,y){
-    console.log(x*y);
-}
-
-let multiplyByTwo=multiply.bind(this,2);
-multiplyByTwo(5);
-
-let multiplyByThree=multiply.bind(this,3);
-multiplyByThree(5);
-
-
-//Method 2
-
-let multiply=function(x){
-    return function(y){
-        console.log(x*y);
+class Student {
+     static count=0;
+     constructor(name,age,phone,marks){
+        this.name=name,
+        this.age=age,
+        this.phone=phone,
+        this.marks=marks
+        Student.count++;
     }
-}
-let multiplyByTwo=multiply(2);
-multiplyByTwo(5);
 
-let multiplyByThree=multiply(3);
-multiplyByThree(5);
-*/
+     eligible() {
+            if(this.marks>=40)
+            console.log(`${this.name} is Eligible`);
+            else
+            console.log(`${this.name} is not Eligible`);
+            
+    }
+   numberOfStudents(){
+   console.log(`There are ${Student.count} Students.`);
+   }
+
+   eligibleForPlacement(minage){
+   return (minmarks)=> {
+
+    if(this.marks>minmarks&&this.age>minage)
+   return console.log(`${this.name} is Eliglible for placement.`);
+    else
+   return console.log(`${this.name} is Not Eliglible for placement.`);
+    
+
+   }
+}
+}
+
+var Jatin=new Student('Jatin',20,1234567891,55);
+var Ram=new Student('Ram',21,4521552554,38);
+var Shyam=new Student('Shyam',18,1535628521,65);
+var Mohan=new Student('Mohan',17,15468464,40);
+var Tarun=new Student('Tarun',19,7895214555,85);
+
+
+Jatin.numberOfStudents();
+Jatin.eligible();
+Ram.eligible();
+Shyam.eligible();
+Mohan.eligible();
+Tarun.eligible();
+Jatin.eligibleForPlacement(18)(40);
+Ram.eligibleForPlacement(18)(40);
+Shyam.eligibleForPlacement(18)(40);
+Mohan.eligibleForPlacement(18)(40);
+Tarun.eligibleForPlacement(18)(40);
